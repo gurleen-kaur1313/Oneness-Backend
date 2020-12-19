21380d155b6eb8d65dcc71fc02c9479117c97d0b
Gurleen Dhillon <gurleendhillonm1114@gmail.com>
2020-12-19 14:50:06 +0530

    changes

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'blog',
     'donations',
     'resources',
+    'ngo',
 ]
 
 MIDDLEWARE = [
--- app/ngo/admin.py
+++ app/ngo/admin.py
@@ -1,4 +1,4 @@
-rom django.contrib import admin
+from django.contrib import admin
 from .models import NGO
 
 admin.site.register(NGO)
