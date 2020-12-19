cebe7ed6732a807f23b0ff6597d06536b6d92df2
Gurleen Dhillon <gurleendhillonm1114@gmail.com>
2020-12-19 18:31:26 +0530

    final

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ EMAIL_HOST_USER = os.environ.get(
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '123456')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
